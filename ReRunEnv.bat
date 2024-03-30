1565e837f23b10ac62138be62f9fd2ad61c432ad
CGladwin <65191901+CGladwin@users.noreply.github.com>
2024-03-30 16:41:19 -0400

    Update ReRunEnv.bat
    add check for virtualenv library

--- ReRunEnv.bat
+++ ReRunEnv.bat
@@ -1,5 +1,6 @@
 @REM This batch file allows for quick restarting of venv
 @RD /S /Q env
+pip install virtualenv
 python -m venv env
 call env\Scripts\activate.bat
 env\Scripts\pip.exe install -r requirements.txt
