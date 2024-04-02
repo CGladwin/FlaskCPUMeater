c2d504d1e8019feb7a88e2beece5043c1f6d54b5
AshwinG-2003 <ashwin.gnanaseelan@opg.com>
2024-04-02 15:02:17 -0400

    Added javascript and css files

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,16 +1,3 @@
-// function fetch_cpu_usage() {
-//     $.get('api/cpu')
-//         .done(function(data) {
-//             console.log(data);
-//             $('#cpu_usage').html(data);
-//             $('#progress_usage').val(data);
-//         })
-//         .fail(function(jqXHR, textStatus, errorThrown) {
-//             console.error('There was a problem with the fetch operation:', errorThrown);
-//         });
-// }
-
-// setInterval(fetch_cpu_usage, 1000);
 function fetch_cpu_usage(){
     $.get('api/cpu', function(data) {
         console.log(data);
